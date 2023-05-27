120c3470d5164893fbaeaa6b6fe6d6c8670a8f1a
1steveee <truckersteve781@gmail.com>
2023-05-26 22:51:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        String message;
+
+        switch (grade) {
+            case "A":
+                message = "Amazing Student";
+                break;
+            case "B":
+                message = "Good Student";
+                break;
+            case "C":
+                message = "Let's study more";
+                break;
+            case "D":
+                message = "You need to work a bit harder";
+                break;
+            case "F":
+                message = "Let's get some tutoring";
+                break;
+            default:
+                message = "Error invalid grade";
+                break;
+        }
+
+        System.out.println(message);
+        
+    }
+}
